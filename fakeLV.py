b7a80677e8530c1b37f79073152f90d527939032
lmartin <lmartin@triumf.ca>
2020-03-23 15:11:39 -0700

    Improved fakeLV

--- fakeLV.py
+++ fakeLV.py
@@ -1,20 +1,22 @@
+#!/usr/bin/env python
 '''
-	Simple socket server using threads
+	Socket server mimicking Nicolas Demers' LabView server
 '''
 
 import socket
 import sys
+import argparse
 
-HOST = 'localhost'	# Symbolic name, meaning all available interfaces
 # HOST = ''	# Symbolic name, meaning all available interfaces
-PORT = 8888	# Arbitrary non-privileged port
 
+## Fake LabView read-only variables
 vars = {
         'RDRotPos' : 4711,
         'RDTransPos' : 12345,
         'RBBusy' : 1
 }
 
+## Fake LabView read/write settings
 settings = {
         'WDRotPos' : 0,
         'WDTransPos' : 0,
@@ -23,6 +25,15 @@ settings = {
 
 
 def answer(msg):
+        """
+        Respond to messages from Midas frontend for LabView.
+
+        Supported commands:
+
+        list_vars to receive a list of available variables
+        <varname>_? to query value of variable <varname>
+        <varname>_<value> to change value of variable <varname>
+        """
         msg = msg.strip("\r\n ")
         print >>sys.stderr, 'received "%s"' % msg
         if(msg == "midas"):
@@ -50,12 +61,18 @@ def answer(msg):
                 else:
                         print "Unknown command:", cmd
 
+
+argparser = argparse.ArgumentParser()
+argparser.add_argument("-H","--host",help="Host for the server socket to be, default localhost",type=str,default="localhost")
+argparser.add_argument("-p","--port",help="Port for the server socket, default 8888",type=int,default=8888)
+args = argparser.parse_args()
+
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 print 'Socket created'
 
 #Bind socket to local host and port
 try:
-	s.bind((HOST, PORT))
+	s.bind((args.host, args.port))
 except socket.error as msg:
 	print 'Bind failed. Error Code : ' + str(msg[0]) + ' Message ' + msg[1]
 	sys.exit()
@@ -63,23 +80,28 @@ except socket.error as msg:
 print 'Socket bind complete'
 
 #Start listening on socket
-s.listen(10)
-print 'Socket now listening'
+try:
+        s.listen(10)
+        print 'Socket now listening on', args.host, ":", args.port
 
-#now keep talking with the client
-while 1:
-    #wait to accept a connection - blocking call
-	conn, addr = s.accept()
-	print 'Connected with ' + addr[0] + ':' + str(addr[1])
-	try:
-                print >>sys.stderr, 'client connected:', addr
-                while True:
-                        data = conn.recv(80)
+        #now keep talking with the client
+        while 1:
+                #wait to accept a connection - blocking call
+	        conn, addr = s.accept()
+	        print 'Connected with ' + addr[0] + ':' + str(addr[1])
+	        try:
+                        print >>sys.stderr, 'client connected:', addr
+                        while True:
+                                data = conn.recv(80)
 
-                        if data:
-                                answer(data)
-                        else:
-                                break
-        finally:
-                conn.close()
+                                if data:
+                                        answer(data)
+                                else:
+                                        break
+                finally:
+                        conn.close()
+except KeyboardInterrupt:
+        s.close()
+        print('Bye!')
+        pass
 s.close()
